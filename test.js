37b590cc726078dcb8d960ddc6057ae44daf1b7f
Paolo Reyes <reyes.paolo@360logix.com>
2018-04-10 12:05:31 +0800

    Oil

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ const test = new Buffer('lat:14.632742\nlong:121.001790\nspeed:0.00\nT:17/06/05 
 
 // console.log(tk103.parseTK103(test));
 // console.log(tk103.parseTK103(smsrawSOSAlert));
-console.log(tk103.parseTK103(smsrawLAC2));
+console.log(tk103.parseTK103(smsrawDoorAlert));
 
 // //
 // console.log(tk103.parseTK103(smsrawOilAlert));
